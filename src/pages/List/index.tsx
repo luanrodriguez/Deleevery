26db875d9e1f9892140b8b35d5be2583833c3756
Luan <luanrodriguesdev@gmail.com>
2022-11-09 17:09:12 -0300

    adicionada pagina com tabela de entregas e o inicio do modal de mapa

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,5 +1,69 @@
+import { useEffect, useState} from "react"
+import Modal from "react-modal"
+import { InputsType } from "../Register"
+import { ListContainer } from "./styles"
+
 export function List() {
+    const [isMapOpen, setIsMapOpen] = useState<boolean>(false)
+    const [listContent, setListContent] = useState<InputsType[]>([])
+    useEffect(() => {
+        setListContent([
+            {
+                origin: 'rua benedito edson correia laranjeira, 31',
+                destination: 'rua hum caragua',
+                clientName: 'Luan',
+                deliveryDate:'09/11/2022'
+            },
+            {
+                origin: 'rua benedito edson correia laranjeira, 31',
+                destination: 'rua hum caragua',
+                clientName: 'Luan',
+                deliveryDate:'09/11/2022'
+            }
+        ])
+    }, [])
+
+    function handleOpenMap() {
+        setIsMapOpen(true)
+    }
+
+    function handleCloseMap() {
+        setIsMapOpen(false)
+    }
+
     return (
-        <h1>Lista</h1>
+        <ListContainer>
+            <Modal 
+                isOpen={isMapOpen}
+                onRequestClose={handleCloseMap}
+                className="map-modal"
+            >
+                <h1>Olá</h1>
+            </Modal>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome do cliente</th>
+                        <th>Data de entrega</th>
+                        <th>Origem</th>
+                        <th>Destino</th>
+                        <th>Rota</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listContent.map((delivery) => {
+                        return (
+                            <tr>
+                                <td>{delivery.clientName}</td>
+                                <td>{delivery.deliveryDate}</td>
+                                <td>{delivery.origin}</td>
+                                <td>{delivery.destination}</td>
+                                <td><button onClick={handleOpenMap}>Mapa</button></td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </ListContainer>
     )
 }
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -1,1 +1,25 @@
 import styled from "styled-components";
+
+export const ListContainer =  styled.div`
+    flex: 1;
+    overflow: auto;
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    th {
+        background-color: #315e8f;
+        padding: 1rem;
+        text-align: left;
+    }
+
+    td {
+        background-color: #4a4a4a;
+        border-top: 4px solid #333333;
+        padding: 1rem;
+    }
+
+    
+`
