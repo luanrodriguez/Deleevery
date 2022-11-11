b049b1bfd054aa4838751663f657a71bde4d365c
luan <luanrodriguesdev@gmail.com>
2022-11-10 23:44:47 -0300

    alterado hashrouter para browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Router from './Router'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <HashRouter>
+    <BrowserRouter>
       <Router />
-    </HashRouter>
+    </BrowserRouter>
 )
